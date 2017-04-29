b8977df9302a44acffb1e43e982cf30a3be74bfe
fadlim <fadlifadly7@gmail.com>
2017-04-29 13:47:34 +0700

    ganti title index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- BEGIN HEAD -->
     <head>
         <meta charset="utf-8"/>
-        <title>SIPRAKERIN</title>
+        <title>SIPRAKERIN tes</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
         <meta content="" name="description"/>
